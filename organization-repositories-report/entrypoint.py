e36dbabe701dc12c7ce9500bf337bea9536cd299
pingmyheart <antoniorussi1972@gmail.com>
2025-02-19 14:53:29 +0100

    [UPDATE] - action

---
+++ organization-repositories-report/entrypoint.py
@@ -0,0 +1,33 @@
+#!/bin/python
+import argparse
+
+from github import Auth
+from github import Github
+
+parser = argparse.ArgumentParser(prog='OrganizationRepositoriesReport',
+                                 description='Make organization repositories report')
+parser.add_argument('-o',
+                    '--organization',
+                    help='Github Organization Name',
+                    type=str)
+parser.add_argument('-t',
+                    '--token',
+                    help='Github Action Token',
+                    type=str)
+args = parser.parse_args()
+
+### Main Login
+auth = Auth.Token(args.token) if args.token else None
+client = Github(auth=auth)
+organization = client.get_organization(args.organization)
+repositories = organization.get_repos()
+repositories = sorted(repositories, key=lambda repository: repository.name.lower())
+content = """"""
+for repo in repositories:
+    content += f"# {repo.name}  \n"
+    content += f"**Name**: {repo.name}  \n"
+    content += f"**URL**: {repo.url}  \n"
+print(content)
+
+with open('/output/report.md', 'w') as file:
+    file.write(content)
